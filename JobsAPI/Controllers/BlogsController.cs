f27a1219a41c95a76833840ac3996d9363e93938
Hamza <hamzararrani77@gmail.com>
2022-10-26 21:39:00 +0530

    Feat: Scaffolded the base blogcontroller for crud operations

---
+++ JobsAPI/Controllers/BlogsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JobsAPI.Models;
+
+namespace JobsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly userDbContext context;
+
+        public BlogsController(userDbContext _context)
+        {
+            context = _context;
+        }
+
+        // GET: api/Blogs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Blog>>> GetBlogs()
+        {
+            return await context.Blogs.ToListAsync();
+        }
+
+        // GET: api/Blogs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Blog>> GetBlog(int id)
+        {
+            var blog = await context.Blogs.FindAsync(id);
+
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return blog;
+        }
+
+        // PUT: api/Blogs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBlog(int id, Blog blog)
+        {
+            if (id != blog.BlogId)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(blog).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BlogExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Blogs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Blog>> PostBlog(Blog blog)
+        {
+            context.Blogs.Add(blog);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBlog", new { id = blog.BlogId }, blog);
+        }
+
+        // DELETE: api/Blogs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBlog(int id)
+        {
+            var blog = await context.Blogs.FindAsync(id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            context.Blogs.Remove(blog);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BlogExists(int id)
+        {
+            return context.Blogs.Any(e => e.BlogId == id);
+        }
+    }
+}
