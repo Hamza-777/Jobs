0b54d907f2aecb2621cd658701366a71e05fcf22
Admin <Admin@DESKTOP-5IBV962>
2022-10-27 15:24:42 +0530

    Feat:UserFrontEnd+Cors

--- JobsFront/src/app/app.module.ts
+++ JobsFront/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { JwtModule } from "@auth0/angular-jwt";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import {HttpClientModule} from '@angular/common/http';
+export function tokenGetter() { 
+  return localStorage.getItem("jwt"); 
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ["localhost:7067"],
+        disallowedRoutes: []
+        
+      }
+    })
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ JobsFront/src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {Router} from '@angular/router'
+import { NgForm } from '@angular/forms';
+import { LoginModel } from '../_interfaces/login.model';
+import { AuthenticatedResponse } from '../_interfaces/authenticatedresponse.model';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  invalidLogin: boolean;
+  credentials: LoginModel = {userdata:'', password:''};
+  constructor(private router: Router,private http:HttpClient) {
+    this.invalidLogin =true;
+   }
+  ngOnInit(): void {
+    
+  }
+  login = ( form: NgForm) => {
+    if (form.valid) {
+      this.http.post<AuthenticatedResponse>("https://localhost:7067/api/auth/login", this.credentials, {
+        headers: new HttpHeaders({ "Content-Type": "application/json"})
+      })
+      .subscribe({
+        next: (response: AuthenticatedResponse) => {
+          const token = response.token;
+          localStorage.setItem("jwt", token); 
+          this.invalidLogin = false; 
+          this.router.navigate(["/"]);
+        },
+        error: (err: HttpErrorResponse) => this.invalidLogin = true
+      })
+    }
+  }
+}
