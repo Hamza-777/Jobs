3a8ecd14e5470cccf3e3f7ed9643a7c42d81d9a4
Vishnukumar <mightyvishnu66@gmail.com>
2022-11-06 20:55:27 +0530

    angular,api course crud

--- JobsFront/src/app/course/course.component.ts
+++ JobsFront/src/app/course/course.component.ts
@@ -1,5 +1,9 @@
 
-import { Component, ElementRef, OnInit,ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Course } from 'src/app/models/course.model';
+import { CoursesService } from 'src/app/services/courses.service';
 
 @Component({
   selector: 'app-course',
@@ -8,34 +12,100 @@ import { Component, ElementRef, OnInit,ViewChild } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
-  @ViewChild('search',{static:true}) searchTerm:ElementRef;
+  
+  courses:Course[]=[];
+  
 
   title = 'mdb-angular-ui-kit-free';
+  search:string = "";
+  flag:number=0;
+
+  
 
   items = ['Action', 'Another action', 'Something else here'];
   coursess=["Business Analysis",'Commercial Law','Human Resources','Accounts','Corporate','Tax Planning',"Machine Learning",'Web Development','Software Development'];
   courseCategories=['Technology','Business Management','Finance Management'];
   filteredItems = this.items;
 
-  searchItems(event: any) {
-    const value = event.target.value;
+  
 
-    this.filterItems(value);
-  }
-
-   
-
-  filterItems(value: string) {
-    const filterValue = value.toLowerCase();
-    this.filteredItems = this.items.filter((item: string) =>
-      item.toLowerCase().includes(filterValue)
-    );
-  }
-
-  constructor() { }
+  constructor(private coursesService:CoursesService,private route:ActivatedRoute, private courseService:CoursesService, private router:Router) { }
 
   ngOnInit(): void {
+
+    this.coursesService.getAllCourses()
+    .subscribe({
+      next:(course)=>{
+
+       
+        this.courses=course;
+        
+        //console.log(this.courses[0]);
+       
+      },
+      error:(response)=>{
+        console.log(response);
+      }
+    })
+    
   }
+  
+
+ 
+
+  Search(){
+
+    
+    this.courses.forEach(element => {
+
+      if(element.courseCategory.toLowerCase().startsWith(this.search.toLowerCase()) && this.flag==0){
+        this.flag=-1;
+        this.search=element.courseCategory;
+        
+      }
+
+      if(element.courseCategory.toLowerCase().endsWith(this.search.toLowerCase()) && this.flag==0){
+        this.flag=-1;
+        this.search=element.courseCategory;
+      }
+
+      if(element.courseName.toLowerCase().startsWith(this.search.toLowerCase()) && this.flag==0){
+        this.flag=1;
+        this.search=element.courseName;
+      }
+
+      
+
+      if(element.courseName.toLowerCase().endsWith(this.search.toLowerCase()) && this.flag==0){
+        this.flag=1;
+        this.search=element.courseName;
+      }
+      
+      
+    });
+      
+    
+
+    if(this.flag==-1){
+      this.router.navigate(['/course-user','listout-courses',this.search])
+    }
+    else if(this.flag==1)
+    {
+      this.router.navigate(['/course-user','goto-course',this.search]);
+    }
+    else
+    {
+      
+       alert("No matches found");
+     
+    }
+
+                
+  
+            
+    }
+
+   
 
 }
 
@@ -97,4 +167,4 @@ export class CourseComponent implements OnInit {
 //       item.toLowerCase().includes(filterValue)
 //     );
 //   }
-// }
+// 
