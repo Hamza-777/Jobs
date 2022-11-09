fb14aaaafb0511c682484e078b936465c5a1e9ba
Hamza <hamzararrani77@gmail.com>
2022-11-09 14:24:15 +0530

    Feat: Shifted jobs filter from api to angular and styled filter options

--- JobsFront/src/app/services/jobs.service.ts
+++ JobsFront/src/app/services/jobs.service.ts
@@ -8,57 +8,37 @@ import { State } from '../_interfaces/State';
 import { Category } from '../_interfaces/Category';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JobsService {
-baseUrl="https://localhost:7067/api/Jobs";
-  constructor(private http:HttpClient) { }
-getAllJobs(cityId?:number, categoryId?:number, stateId?:number, sortName?:string, search?:string){
-  let params= new HttpParams();
-  if(categoryId){
-    params= params.append('categoryId', categoryId.toString());
-  }
-  if(cityId){
-    params= params.append('cityId', cityId.toString());
-  }
-  if(stateId){
-    params= params.append('stateId', stateId.toString());
-  }
-  if(sortName != null){
-    params= params.append('sort', sortName);
-
+  baseUrl = 'https://localhost:7067/api/Jobs';
+  constructor(private http: HttpClient) {}
+  getAllJobs() {
+    return this.http.get<Job[]>(this.baseUrl, { observe: 'response' }).pipe(
+      map((response) => {
+        return response.body;
+      })
+    );
   }
 
-  if(search != null){
-    params=params.append('search', search);
+  getAllJobsById(id: number) {
+    return this.http.get<Job>(this.baseUrl + '/' + id);
   }
 
-  return this.http.get<Job[]>(this.baseUrl, {observe: 'response', params}).pipe(
-    map(response=>{
-      return response.body;
-    })
-  )
-}
+  getAllCity() {
+    return this.http.get<City[]>(this.baseUrl + '/city');
+  }
 
-getAllJobsById(id:number){
-  return this.http.get<Job>(this.baseUrl+"/"+id);
-}
-
-getAllCity(){
-  return this.http.get<City[]>(this.baseUrl+"/city");
-}
-
-getAllState(){
-  return this.http.get<State[]>(this.baseUrl+"/state");
-}
-getAllCategory(){
-  return this.http.get<Category[]>(this.baseUrl+"/category");
-}
-postJobs(postJobRequest:Job):Observable<Job>{
-  return this.http.post<Job>(this.baseUrl,postJobRequest);
-}
-editJobs(id:number, editedJobRequest:Job){
-  return this.http.put<Job>(this.baseUrl +"/"+id, editedJobRequest);
-}
-
+  getAllState() {
+    return this.http.get<State[]>(this.baseUrl + '/state');
+  }
+  getAllCategory() {
+    return this.http.get<Category[]>(this.baseUrl + '/category');
+  }
+  postJobs(postJobRequest: Job): Observable<Job> {
+    return this.http.post<Job>(this.baseUrl, postJobRequest);
+  }
+  editJobs(id: number, editedJobRequest: Job) {
+    return this.http.put<Job>(this.baseUrl + '/' + id, editedJobRequest);
+  }
 }
